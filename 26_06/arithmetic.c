ec4c0e53082bbc97de241b61380f53b99b57e1c4
RohanPoloju899 <polojurohan1@gmail.com>
2026-06-09 20:51:01 +0530

    Create arithmetic.c

---
+++ 26_06/arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    int a=5,b=2;
+
+    // binary arithmetic operators
+    printf("sum of %d and %d is %d \n",a,b,a+b); // additon operator
+    printf("subtration of %d and %d is %d \n",b,a,a-b); //subtraction operator
+    printf("product of %d and %d is %d \n",a,b,a+b); //multiplcation operator
+    printf("quotient of %d is divided by %d is %d \n",a,b,a/b); //remainder operator
+    printf("remainder when %d is divided by %d is %d \n",a,b,a%b); //modulus operator
+    
+}
